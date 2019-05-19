8207c0a06283473453adb4110d81f5c7b667560b
EmanuelECL12 <48462036+EmanuelECL12@users.noreply.github.com>
2019-05-19 14:33:09 -0500

    aqui estoy haciendo lo de curiosidades

---
+++ app/src/main/java/com/example/tabla_periodica/Curiosidades.java
@@ -0,0 +1,13 @@
+package com.example.tabla_periodica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Curiosidades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curiosidades);
+    }
+}
